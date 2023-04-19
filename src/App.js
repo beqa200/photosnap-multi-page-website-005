91c3b7d28884518568335ceb8652a325b5238b0e
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-04-19 22:53:28 +0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Features from "./pages/Features";
+import Pricing from "./pages/Pricing";
+import Stories from "./pages/Stories";
+import Home from "./pages/Home";
+import {Routes, Route} from "react-router-dom";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/stories" element={<Stories/>} />
+        <Route path="/features" element={<Features/>} />
+        <Route path="/pricing" element={<Pricing/>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
