bea706b4028219d5c5081ec4de5704218f9a8879
giorgisilagadze <silagadze.gio123@gmail.com>
2023-04-25 18:23:12 +0400

    style: part of images at home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Image from '../components/Image'
 
+const minContent = [
+  {
+    bg: "./assets/stories/mobile/mountains.jpg",
+    date: "April 16th 2020",
+    title: "The Mountains",
+    creator: "by John Appleseed"
+  },
+  {
+    bg: "./assets/stories/mobile/cityscapes.jpg",
+    date: "April 14th 2020",
+    title: "Sunset Cityscapes",
+    creator: "by Benjamin Cruz"
+  },
+  {
+    bg: "./assets/stories/mobile/18-days-voyage.jpg",
+    date: "April 11th 2020",
+    title: "18 Days Voyage",
+    creator: "by Alexei Borodin"
+  },
+  {
+    bg: "./assets/stories/mobile/architecturals.jpg",
+    date: "April 9th 2020",
+    title: "Architecturals",
+    creator: "by Samantha Brooke"
+  },
+]
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      {minContent.map((item) => (
+        <Image bg={item.bg} dateTxt={item.date} title={item.title} creator={item.creator} key={Math.random()}/>
+      ))}
+    </div>
   )
 }
 
