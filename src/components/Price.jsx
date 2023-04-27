cccd72127275fcb8940fdf5eb83af1f76e1d79e8
giorgisilagadze <silagadze.gio123@gmail.com>
2023-04-27 19:58:44 +0400

    style: offer content is done

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -6,7 +6,32 @@ import {
   Year,
   Switch,
   Circle,
+  OfferDiv,
+  Offertitle,
+  OfferCond,
+  OfferPrice,
+  OfferDur,
+  ColorDiv,
+  Butt,
 } from "../styled-components/Price.styled";
+
+const offer = [
+  {
+    title: "Basic",
+    off: "Includes basic usage of our platform. Recommended for new and aspiring photographers.",
+    butTxt: "PICK PLAN",
+  },
+  {
+    title: "Pro",
+    off: "More advanced features available. Recommended for photography veterans and professionals.",
+    butTxt: "PICK PLAN",
+  },
+  {
+    title: "Business",
+    off: "Additional features available such as more detailed metrics. Recommended for business owners.",
+    butTxt: "PICK PLAN",
+  },
+];
 
 export default function Price() {
   const [isChecked, setIsChecked] = useState(false);
@@ -18,15 +43,47 @@ export default function Price() {
   return (
     <PriceMain>
       <Period>
-        <Month>Monthly</Month>
+        <Month op={isChecked ? ".5" : "1"}>Monthly</Month>
         <Switch onClick={change} bg={isChecked ? "#000000" : "#DFDFDF"}>
           <Circle
             bgC={isChecked ? "#DFDFDF" : "#000000"}
             mLeft={isChecked ? "32px" : null}
           />
         </Switch>
-        <Year>Yearly</Year>
+        <Year op={isChecked ? "1" : ".5"}>Yearly</Year>
       </Period>
+      {offer.map((item, index) => {
+        return (
+          <OfferDiv
+            key={Math.random()}
+            bgColor={index === 1 ? "#000000" : "#F5F5F5"}
+            pt={index === 1 ? "48px" : "56px"}
+            pb={index === 1 ? "48px" : "40px"}
+          >
+            {index === 1 ? <ColorDiv /> : null}
+            <Offertitle cl={index === 1 ? "#FFFFFF" : "#000000"}>
+              {item.title}
+            </Offertitle>
+            <OfferCond cltxt={index === 1 ? "#FFFFFF" : "#000000"}>
+              {item.off}
+            </OfferCond>
+            <OfferPrice clpr={index === 1 ? "#FFFFFF" : "#000000"}>
+              {index === 0 ? (isChecked ? "$190.00" : "$19.00") : null}
+              {index === 1 ? (isChecked ? "$390.00" : "$39.00") : null}
+              {index === 2 ? (isChecked ? "$990.00" : "$99.00") : null}
+            </OfferPrice>
+            <OfferDur clDur={index === 1 ? "#FFFFFF" : "#000000"}>
+              {isChecked ? "per year" : "per month"}
+            </OfferDur>
+            <Butt
+              bgButt={index === 1 ? "#FFFFFF" : "#000000"}
+              clButt={index === 1 ? "#000000" : "#FFFFFF"}
+            >
+              {item.butTxt}
+            </Butt>
+          </OfferDiv>
+        );
+      })}
     </PriceMain>
   );
 }
--- src/styled-components/Price.styled.js
+++ src/styled-components/Price.styled.js
@@ -10,7 +10,7 @@ export const Period = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0px 32px;
+    padding: 0px 32px 16px;
 `
 
 export const Month = styled.h1`
@@ -18,6 +18,7 @@ export const Month = styled.h1`
     font-size: 18px;
     line-height: 25px;
     color: #000000;
+    opacity: ${({op}) => op};
 `
 
 export const Year = styled.h1`
@@ -25,7 +26,7 @@ export const Year = styled.h1`
     font-size: 18px;
     line-height: 25px;
     color: #000000;
-    opacity: .5;
+    opacity: ${({op}) => op};
 `
 
 export const Switch = styled.div`
@@ -47,3 +48,73 @@ export const Circle = styled.div`
     transition: .2s;
 `
 
+export const OfferDiv = styled.div`
+    width: 100%;
+    padding: ${({pt}) => pt} 21.5px ${({pb}) => pb};
+    background-color: ${({bgColor}) => bgColor};
+    margin-top: 24px;
+    position: relative;
+`
+
+export const Offertitle = styled.h1`
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 25px;
+    text-align: center;
+    color: ${({cl}) => cl};
+    margin-top: ${({marTop}) => marTop};
+`
+
+export const OfferCond = styled.p`
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    text-align: center;
+    opacity: 0.6;
+    color: ${({cltxt}) => cltxt};
+    margin-top: 18px;
+
+`
+
+export const OfferPrice = styled.p`
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 48px;
+    letter-spacing: 4.17px;
+    text-align: center;
+    color: ${({clpr}) => clpr};
+    margin-top: 40px;
+`
+
+export const OfferDur = styled.p`
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    text-align: center;
+    opacity: 0.6;
+    color: ${({clDur}) => clDur};
+    margin-top: 0px;
+`
+
+export const ColorDiv = styled.div`
+    width: 100%;
+    height: 6px;
+    background: linear-gradient(26.57deg, #FFC593 0%, #BC7198 43.29%, #5A77FF 83.33%);
+    left: 0px;
+    position: absolute;
+    top: 0px;
+`
+
+export const Butt = styled.button`
+    width: 245px;
+    height: 40px;
+    background-color: ${({bgButt}) => bgButt};
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 15.62px;
+    letter-spacing: 2px;
+    color: ${({clButt}) => clButt};
+    margin: 40px auto 0px;
+    border: none;
+    display: block;
+`
